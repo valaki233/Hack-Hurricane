c88204e20f3d7379acb373c2df7c4b947b26fd5d
Nagy Ábel <abel.nagy2601@gmail.com>
2024-01-04 16:53:54 +0100

    added all places, recommended place gets forwarded

--- public/map.js
+++ public/map.js
@@ -1,95 +1,62 @@
-
-// const pData = { key1: 'value1', key2: 'value2' };
-
-// fetch('/getroute', {
-//     method: 'POST',
-//     headers: {
-//         'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify(pData),
-// })
-// .then(response => response.json())
-// .then(data => console.log(data))
-// .catch((error) => console.error('Error:', error));
-
-
-// const data = json.routes[0];
-// const route = data.geometry.coordinates;
-// const geojson = {
-// type: 'Feature',
-// properties: {},
-// geometry: {
-//     type: 'LineString',
-//     coordinates: route
-// }
-// };
-// if (map.getSource('route')) {
-// map.getSource('route').setData(geojson);
-// } else {
-// map.addLayer({
-//     id: 'route',
-//     type: 'line',
-//     source: {
-//     type: 'geojson',
-//     data: geojson
-//     },
-//     layout: {
-//     'line-join': 'round',
-//     'line-cap': 'round'
-//     },
-//     paint: {
-//     'line-color': '#3887be',
-//     }
-// });
-// }
-
 mapboxgl.accessToken = 'pk.eyJ1IjoidmFsYWtpIiwiYSI6ImNscXk5ajJzNDBtc2wyanBjYTVjNDR6NjcifQ.tV3Rh4n0ZrApBUotAZe-yQ';
 const map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/mapbox/streets-v12',
-  center: [-122.662323, 45.523751], // starting position
-  zoom: 12
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v12',
+    center: [-117.918911, 33.812373], // starting position
+    zoom: 15
 });
-// create a function to make a directions request
-async function getRoute(end) {
-    // make a directions request using cycling profile
-    // an arbitrary start will always be the same
-    // only the end or destination will change
-    const query = await fetch(
-      `https://api.mapbox.com/directions/v5/mapbox/cycling/${start[0]},${start[1]};${end[0]},${end[1]}?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`,
-      { method: 'GET' }
+
+fetch('/attractions')
+.then(async response => await response.json())
+.then(attractions => {
+    console.log(attractions.attractions);
+    const fetchy = attractions.attractions.map(attraction => {
+        const latitude = attraction.lat;
+        const longitude = attraction.long;
+        console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+        return `${longitude},${latitude}`; // Swap longitude and latitude positions
+    }).join(';'); // Join the array elements with a semicolon
+    
+    console.log(typeof(attractions));
+    
+    console.log(fetchy);
+    return fetch(
+        `https://api.mapbox.com/directions/v5/mapbox/cycling/${fetchy}?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`,
+        { method: 'GET' }
     );
-    const json = await query.json();
-    const data = json.routes[0];
-    const route = data.geometry.coordinates;
-    const geojson = {
-      type: 'Feature',
-      properties: {},
-      geometry: {
-        type: 'LineString',
-        coordinates: route
-      }
-    };
-    // if the route already exists on the map, we'll reset it using setData
-    if (map.getSource('route')) {
-      map.getSource('route').setData(geojson);
-    }
-    // otherwise, we'll make a new request
-    else {
-      map.addLayer({
-        id: 'route',
-        type: 'line',
-        source: {
-          type: 'geojson',
-          data: geojson
-        },
-        layout: {
-          'line-join': 'round',
-          'line-cap': 'round'
-        },
-        paint: {
-          'line-color': '#3887be'
+})
+    .then(response => response.json())
+    .then(data => {
+        const route = data.routes[0].geometry.coordinates;
+        const geojson = {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+                type: 'LineString',
+                coordinates: route
+            }
+        };
+        // if the route already exists on the map, we'll reset it using setData
+        if (map.getSource('route')) {
+            map.getSource('route').setData(geojson);
+        } else { // otherwise, we'll add it to the map
+            map.addLayer({
+                id: 'route',
+                type: 'line',
+                source: {
+                    type: 'geojson',
+                    data: geojson
+                },
+                layout: {
+                    'line-join': 'round',
+                    'line-cap': 'round'
+                },
+                paint: {
+                    'line-color': '#3887be',
+                    'line-width': 5,
+                    'line-opacity': 0.75
+                }
+            });
         }
-      });
-    }
-}
+    })
+    .catch(error => console.error('Error:', error));
--- server.js
+++ server.js
@@ -8,9 +8,10 @@ const express = require('express');
 const db = require('./db.js');
 const openai = require('openai');
 const mapbox = require('mapbox');
+const { json } = require('body-parser');
 const app = express();
 const port = 3000;
-
+var rec_list = json();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -30,9 +31,9 @@ app.post('/register', async (req, res) => {
   const getRec = async (attractions, data) => {
     const dataString = JSON.stringify(data);
     const attractionsString = JSON.stringify(attractions);
-    const prompt = `You are a professional theme park adviser and you need to decide where your client should go. The attractions are the following: '${attractionsString}'. And your client data pulled from a database is ${dataString}. Only return a json with the attractions in order of logic, for example dont put restaurant before a rollercoster. It should fill out the whole day, pay attention to put eating in the correct times`;
+    const prompt = `You are a professional theme park adviser and you need to decide where your client should go. The attractions are the following: '${attractionsString}'. And your client data pulled from a database is ${dataString}. Only return a json with the attractions in order of logic, for example dont put restaurant before a rollercoster. It should fill out the whole day, max 10 attractions, pay attention to put eating in the correct times`;
     console.log(prompt);
-    const sysprompt = `You are ChatGPT, a large language model trained by OpenAI. Answer as concisely and shortly as possible. Only return the stops in json format nothing else no text no comments no additional information, the json's list name must be 'attractions'! , Knowledge cutoff: 2021-September`
+    const sysprompt = `You are ChatGPT, a large language model trained by OpenAI. Answer as concisely and shortly as possible. Only return the stops in json format nothing else no text no comments no additional information, VERY IMPORTANT IS THAT YOU SHOULD NOT PUT OTHER TEXT IN IT the json's list name must be 'attractions'! , Knowledge cutoff: 2021-September`
     const ai = new openai.OpenAI({apiKey: process.env.OPENAI_API_KEY});
   
     const completion = await ai.chat.completions.create({
@@ -45,15 +46,11 @@ app.post('/register', async (req, res) => {
   try {
     var formdata = req.body;
     var attractions = await db.Attractions.findAll();
-    console.log(attractions);
     await getRec(attractions, formdata)
-      .then(res => () => {
-        // var data = JSON.parse(res);
-        // var client = new mapbox(process.env.MAPBOX_TOKEN);
-        console.log(res);
-      })
+      .then(res => rec_list = JSON.parse(res))
       .catch(error => console.error(error));
 
+    console.log(typeof(rec_list));
     res.status(200).redirect('/map.html');
   } catch(err) {
     console.error(err);
@@ -63,20 +60,15 @@ app.post('/register', async (req, res) => {
 
 
 
-app.get('/getroute', (req, res) => {
-    console.log(req.body);
-    async function getRoute(ai_resp) { 
-        let route = '';
-        for (let i = 0; i < points.length; i++) {
-          route += `${points[i][0]},${points[i][1]};`;
-        }
-        const query = await fetch(
-          `https://api.mapbox.com/directions/v5/mapbox/cycling/${route}?steps=true&geometries=geojson&access_token=${process.env.MAPBOX_TOKEN}`,
-          { method: 'GET' }
-        );
-        }
-      })
+app.get('/attractions', async (req, res) => { 
+  try {
+    res.status(200).json(rec_list);
+  } catch(err) {
+    console.error(err);
+    res.status(500).json({ error: 'An error occurred while getting attractions' });
+  }
 
+});
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
     console.log(process.env.ENV_TEST);
